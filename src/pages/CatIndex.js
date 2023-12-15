8d2913ee950eb6de6674be8f56e641778f59a037
LEARN Academy <learnacademy@MacBook-Air.local>
2023-12-14 22:57:21 -0800

    style update 2

--- src/pages/CatIndex.js
+++ src/pages/CatIndex.js
@@ -1,38 +1,34 @@
-import { NavLink } from "react-router-dom"
-import { Card, CardBody, CardTitle, CardSubtitle, Button } from "reactstrap"
+import { NavLink } from "react-router-dom";
+import { Card, CardBody, CardTitle, CardSubtitle } from "reactstrap";
 
 const CatIndex = ({ cats }) => {
   return (
     <div className="bg">
-    <div className="cards-index">
-      {cats?.map((cat, index) => {
-        return (
-          <Card
-            style={{
-              width: "14rem"
-            }}
-            key={index}
-          >
-            <img alt={`Welcome to ${cat.name}'s Profile`} 
-            src={cat.image} 
-            />
-            <CardBody>
-              <CardTitle tag="h5">
-                {cat.name}
-                </CardTitle>
-              <CardSubtitle className="mb-2 text-muted" tag="h6">
-                Age: {cat.age}
-              </CardSubtitle>
-              <NavLink to={`/catshow/${cat.id}`}>
-                {cat.name}'s' profile
+      <div className="cards-index">
+        {cats?.map((cat, index) => {
+          return (
+            <Card
+              style={{
+                width: "14rem",
+              }}
+              key={index}
+            >
+              <img alt={`Welcome to ${cat.name}'s Profile`} src={cat.image} />
+              <CardBody>
+                <CardTitle tag="h5">{cat.name}</CardTitle>
+                <CardSubtitle className="mb-2 text-muted" tag="h6">
+                  Age: {cat.age}
+                </CardSubtitle>
+                <NavLink to={`/catshow/${cat.id}/${cat.name}/${cat.age}`}>
+                  {cat.name}'s' profile
                 </NavLink>
-            </CardBody>
-          </Card>
-        )
-      })}
+              </CardBody>
+            </Card>
+          );
+        })}
       </div>
-      </div>
-  )
-  }
+    </div>
+  );
+};
 
-export default CatIndex
+export default CatIndex;
--- src/pages/CatShow.js
+++ src/pages/CatShow.js
@@ -1,44 +1,43 @@
-import React from "react"
+import React from "react";
 import { useParams, NavLink } from "react-router-dom";
 
-
 const CatShow = ({ cats, deleteCat }) => {
-  const { id } = useParams ()
-  let selectedCat = cats?.find((cat) => cat.id === +id)
-  console.log(selectedCat)
-  
+  const { id, name, age } = useParams();
+  let selectedCat = cats?.find((cat) => cat.id === +id);
 
   const handleDelete = () => {
-    const confirmDelete = window.confirm("Are you sure you want to delete this cat?");
+    const confirmDelete = window.confirm(
+      "Are you sure you want to delete this cat?"
+    );
     if (confirmDelete) {
       deleteCat(selectedCat.id);
     }
-  }
-
+  };
 
   return (
-
-<div className="image-structure">
+    <div className="image-structure">
       {selectedCat && (
-      <>
-        <NavLink to={`/catedit/${selectedCat.id}`} className="edit-button">
-        Edit Cat
-        </NavLink>
-        <img alt={`${selectedCat.name}'s profile`} src={selectedCat.image} />
-        <h3 className="cat-info">{selectedCat.enjoys}</h3>
-
-
-      </>
+        <>
+          <NavLink to={`/catedit/${selectedCat.id}`} className="edit-button">
+            Edit Cat
+          </NavLink>
+          <img alt={`${selectedCat.name}'s profile`} src={selectedCat.image} />
+          <div>
+            <div className="name-age">
+            <p>Name: {name}</p>
+            <p>Age: {age}</p>
+            </div>
+          <h3 className="cat-info">{selectedCat.enjoys}</h3>
+          </div>
+        </>
       )}
-        <NavLink to="/catindex">
+      <NavLink to="/catindex">
         <button onClick={handleDelete} className="delete-button">
-            Delete Cat
-          </button>
-        </NavLink>
-</div>
-  )
-}
+          Delete Cat
+        </button>
+      </NavLink>
+    </div>
+  );
+};
 
-
-
-export default CatShow
+export default CatShow;
